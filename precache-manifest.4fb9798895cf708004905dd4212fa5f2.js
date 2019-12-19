997d1f8472e83519e9a51621ab4021dc664905b5
Rahim Virani <rahz616@gmail.com>
2019-12-19 17:14:00 -0500

    Updates

---
+++ precache-manifest.4fb9798895cf708004905dd4212fa5f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e66ff0f96659cdfb0018b0d301a8156",
+    "url": "/legacy-moveset/index.html"
+  },
+  {
+    "revision": "b379c72928aa444deab2",
+    "url": "/legacy-moveset/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "d14fecfa75c8af1318e8",
+    "url": "/legacy-moveset/static/css/main.f646e8f7.chunk.css"
+  },
+  {
+    "revision": "b379c72928aa444deab2",
+    "url": "/legacy-moveset/static/js/2.09a525f5.chunk.js"
+  },
+  {
+    "revision": "dd30ad2c00174e59bb32191ea757f584",
+    "url": "/legacy-moveset/static/js/2.09a525f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d14fecfa75c8af1318e8",
+    "url": "/legacy-moveset/static/js/main.c11d2b9f.chunk.js"
+  },
+  {
+    "revision": "2bf2a646e1a5e2b509fe",
+    "url": "/legacy-moveset/static/js/runtime-main.460b707b.js"
+  }
+]);
